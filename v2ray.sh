2e40d09bc8e9b82d7b85201ed61579c7a4d0aa18
Scaleya <61817665+Scaleya@users.noreply.github.com>
2020-06-17 14:50:46 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -149,7 +149,7 @@ create_vmess_URL_config() {
 		cat >/etc/v2ray/vmess_qr.json <<-EOF
 		{
 			"v": "2",
-			"ps": scaleya.com_${ip}",
+			"ps": "scaleya.com_${ip}",
 			"add": "${ip}",
 			"port": "${v2ray_port}",
 			"id": "${v2ray_id}",
